ee230c3f10f6674f3a71e597c9c5d8d3cdc2604e
Christian <christian.mendez05@utrgv.edu>
2023-11-29 16:29:48 -0600

    Added front end code

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-#First commit
-print("hello word")
-
+# Import libraries and packages, they contain additional functions.
+from flask import Flask, render_template
+# Create web application using Flask
+app = Flask(__name__)
+# Create our routes
+@app.route('/')
+def home():
+ return render_template('index.html')
+# Start the web application
+app.run(
+ debug=True
+)
